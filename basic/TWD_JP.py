16d4ec9bfebb3de62da5d309e00cc9fb847b56d1
Maru <3q.cindy@gmail.com>
2025-05-28 20:00:03 +0800

    feature.basic exercise

---
+++ basic/TWD_JP.py
@@ -0,0 +1,7 @@
+from math import floor
+
+
+TWD = input("請輸入台幣金額：")
+JP = float(TWD)*4.38
+
+print("日幣為：" + str(JP))
---
+++ basic/cal.py
@@ -0,0 +1,10 @@
+# 日期
+import calendar
+from datetime import datetime
+
+now = datetime.now()
+
+yy = now.year
+mm = now.month
+
+print(calendar.month(yy,mm))
